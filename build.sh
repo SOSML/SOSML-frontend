60e780c98f35adb339ce0677b016927802fcab0c
Jesko <jeskod@gmail.com>
2017-09-16 21:20:32 +0200

    minor improvements

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 cd ./submodules/interpreter
 npm run dist
 cd ../webworker
-npm run build
+npm run dist
 cd ../../
 cp ./submodules/interpreter/build/interpreter.min.js ./frontend/public/interpreter.js
 cp ./submodules/webworker/build/webworker.js ./frontend/public/webworker.js
