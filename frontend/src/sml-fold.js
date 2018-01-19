b12c03797f19bae630bfddd57264f7f514ef6331
SecureAB <secureab-github@outlook.com>
2018-01-19 01:01:59 +0100

    Allow folding of comments

--- frontend/src/sml-fold.js
+++ frontend/src/sml-fold.js
@@ -18,7 +18,7 @@
         var text = cm.getLine(lineNo);
         var spaceTo = text.search(/\S/);
 
-        if (spaceTo === -1 || /\bcomment\b/.test(cm.getTokenTypeAt(CodeMirror.Pos(lineNo, spaceTo + 1)))) {
+        if (spaceTo === -1) {
             return -1;
         }
         return CodeMirror.countColumn(text, null, cm.getOption("tabSize"))
