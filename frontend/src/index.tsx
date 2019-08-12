fb18f4e87ed2447a33350b38b4ef97c3e028ad7d
Philip Wellnitz <philipwellnitz@gmx.de>
2019-08-13 05:35:00 +0900

    add proper service worker

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import RootPage from './components/RootPage';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <RootPage />,
     document.getElementById('root') as HTMLElement
 );
+
+registerServiceWorker();
