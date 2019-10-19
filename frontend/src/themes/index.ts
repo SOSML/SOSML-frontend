89b3be222a89e92cb387037a3794991816f8c27a
SohnyBohny <sohny.bean@streber24.de>
2019-10-19 19:01:44 +0200

    Add: otaku theme

--- frontend/src/themes/index.ts
+++ frontend/src/themes/index.ts
@@ -6,6 +6,7 @@ import { THEME as THEME_MADOKA } from './madoka';
 import { THEME as THEME_SAYAKA } from './sayaka';
 import { THEME as THEME_HOMURA } from './homura';
 import { THEME as THEME_MAMI } from './mami';
+import { THEME as THEME_OTAKU } from './otaku';
 
 export let THEMES: ThemeCollection = {
     'yachiyo':  THEME_YACHIYO,
@@ -14,4 +15,5 @@ export let THEMES: ThemeCollection = {
     'sayaka':   THEME_SAYAKA,
     'homura':   THEME_HOMURA,
     'mami':     THEME_MAMI,
+    'otaku':    THEME_OTAKU,
 };
